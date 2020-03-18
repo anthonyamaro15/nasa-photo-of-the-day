917b3864c79d8a3255406c97db7cfe1e7c8a8a20
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-03-18 15:16:11 -0700

    working on the stretch

--- src/components/Information.js
+++ src/components/Information.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { withFormik, Form, Field } from "formik";
+import * as yup from "yup";
 
-const Information = ({ data }) => {
+const Information = ({ data, changeDate }, { status }) => {
   const { copyright, date, explanation, title, url } = data;
+
+  useEffect(() => {
+    status && changeDate(status);
+    console.log(status);
+  }, [status]);
 
   return (
     <div className="container">
-      <h1>{title}</h1>
+      <Form>
+        <Field component="select" name="dates">
+          <option value="2000-02-14">2000-02-14</option>
+          <option value="2010-02-24">2010-02-24</option>
+          <option value="2006-11-01">2006-11-01</option>
+        </Field>
+        <button type="submit">Submit</button>
+      </Form>
       <div className="card-container">
+        <h1>{title}</h1>
         <img src={url} />
         <p className="desc">{explanation}</p>
         <span className="date">{date}</span>
@@ -16,4 +31,13 @@ const Information = ({ data }) => {
   );
 };
 
-export default Information;
+export default withFormik({
+  mapPropsToValues: () => ({
+    dates: ""
+  }),
+  handleSubmit: (values, { resetForm, setStatus }) => {
+    //  console.log(values);
+    setStatus(values);
+    resetForm();
+  }
+})(Information);
--- src/components/MainApp.js
+++ src/components/MainApp.js
@@ -5,32 +5,32 @@ import axios from "axios";
 const MainApp = () => {
   const [data, setData] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
-  //   const apiKey = "Jz0JKubQ4OawzIo1KDsb4C9Z9dqbh3FC56Pqz7FU";
-
-  //   useEffect(() => {
-  //     axios
-  //       .get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
-  //       .then(res => {
-  //         console.log(res.data);
-  //         setData(res.data);
-  //         setIsLoaded(true);
-  //       })
-  //       .catch(err => console.log(err));
-  //   }, []);
+  const [date, setDate] = useState("2012-03-24");
+  const apiKey = "Jz0JKubQ4OawzIo1KDsb4C9Z9dqbh3FC56Pqz7FU";
 
   useEffect(() => {
-    const newData = JSON.parse(localStorage.getItem("data"));
-    if (newData) {
-      setData(newData);
-      setIsLoaded(true);
-    }
-  }, []);
-  useEffect(() => {
-    localStorage.setItem("data", JSON.stringify(data));
-  }, [data]);
+    axios
+      .get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`)
+      .then(res => {
+        setData(res.data);
+        setIsLoaded(true);
+      })
+      .catch(err => console.log(err));
+  }, [date]);
+
+  const changeDate = dateInfo => {
+    setDate(dateInfo);
+    console.log("new date", dateInfo);
+  };
 
   return (
-    <div>{isLoaded ? <Information data={data} /> : <h1>Loading...</h1>}</div>
+    <div>
+      {isLoaded ? (
+        <Information data={data} changeDate={changeDate} />
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </div>
   );
 };
 
